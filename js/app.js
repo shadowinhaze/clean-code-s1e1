c9ec192ec25a051149012ef73de66e94bc812058
shadowinhaze <shadowinhaze@gmail.com>
2021-11-26 18:11:32 +0100

    refactor: moves the project files into folders, changes the corresponding links (acc. rule 6 in generic-principles.md)

--- js/app.js
+++ js/app.js
@@ -44,7 +44,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="edit";
 
     deleteButton.className="delete";
-    deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.src='./assets/icons/remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
 
